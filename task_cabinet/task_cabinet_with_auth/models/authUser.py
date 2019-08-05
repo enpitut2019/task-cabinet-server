f43d64b21e06e1f4c4865e637ac6c5340a3c1f95
MokkeMeguru <meguru.mokke@gmail.com>
2019-08-05 09:07:36 +0900

    apply custom user
    custom user として task_cabinet_with_auth を追加し適用しました。c

---
+++ task_cabinet/task_cabinet_with_auth/models/authUser.py
@@ -0,0 +1,100 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.validators import MinLengthValidator
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.core.mail import send_mail
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError('Invalid Value: Please Input your User Name')
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser', True) is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        return self._create_user(username, email, password, **extra_fields)
+
+
+# id cannot be removed since Too complex framework dependencies
+class AuthUser(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _('username'),
+        max_length=30,
+        unique=True,
+        help_text=_('Required. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator,
+                    MinLengthValidator(5)],
+        error_messages={
+            'unique': _("A user with that username already exists.")
+        })
+    email = models.EmailField(_('email address'), blank=False)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'),
+    )
+    is_superuser = models.BooleanField(
+        _('superuser status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'),
+    )
+    last_login = models.EmailField(_('email address'), blank=True)
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_('Designates whether this user should be treated as active.'
+                    'Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    first_name = models.CharField(default="", blank=True, max_length=1)
+    last_name = models.CharField(default="", blank=True, max_length=1)
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'password']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        return "Not Implemented"
+
+    def get_short_name(self):
+        return "Not Implemented"
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        send_mail(subject, message, from_email, [self.email], **kwargs)
