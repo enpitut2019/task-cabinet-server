bce821c2effa5a76dd96ba39430acb4e8cf4bfe3
MokkeMeguru <meguru.mokke@gmail.com>
2019-08-05 09:14:36 +0900

    add authTask

--- task_cabinet/task_cabinet_with_auth/admin.py
+++ task_cabinet/task_cabinet_with_auth/admin.py
@@ -3,5 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 from .models.authUser import AuthUser
+from .models.authTask import AuthTask
 
 admin.site.register(AuthUser, UserAdmin)
+admin.site.register(AuthTask)
